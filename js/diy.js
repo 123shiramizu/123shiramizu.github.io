fa3cb62b6edd9f06cb7365232516ffd5b71edfa8
刺猬鱼 <90460038+123shiramizu@users.noreply.github.com>
2023-01-26 02:36:42 +0800

    Add files via upload

---
+++ js/diy.js
@@ -0,0 +1,15 @@
+$(function(){
+    $('#dowebok').fullpage({
+        'navigation': true,
+        'navigationPosition':"left",
+        'navigationColor':['#fff'],
+
+    });
+});
+// var btn=document.querySelector('#mer')
+// // var hz=document.querySelector('.ly-txt14')
+// var right2=document.querySelector('.right2')
+// if(right2.style.top==='-80%'){
+//     console.log('1')
+// 	btn.style.display='block'
+// }
